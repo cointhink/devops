a0dfd7caca58c537a2ae8aee86bc5c1dae9fe7b8
Don Park <donp@donp.org>
2017-07-12 13:21:40 -0700

    go to NJ and mount the disk

--- terraform/cloud.tf
+++ terraform/cloud.tf
@@ -1,3 +1,8 @@
+variable "region" {
+  default = 1 # New Jersey
+#  default = 4 # Seattle
+}
+
 resource "vultr_ssh_key" "cointhink" {
   name = "root ssh key"
   public_key = "${file("root_ssh_pub.key")}"
@@ -5,7 +10,7 @@ resource "vultr_ssh_key" "cointhink" {
 
 resource "vultr_server" "cointhink_web_1" {
   name = "cointhink web 1"
-  region_id = 4 # Seattle
+  region_id = "${var.region}"
   os_id = 231 #	Ubuntu 16.10
   plan_id = 201 # $5/mo,1GB ram, 25GB disk
 
@@ -16,7 +21,7 @@ resource "vultr_server" "cointhink_web_1" {
 
 resource "vultr_server" "cointhink_algo_1" {
   name = "cointhink algo 1"
-  region_id = 4 # Seattle
+  region_id = "${var.region}"
   os_id = 231 #	Ubuntu 16.10
   plan_id = 201 # $5/mo,1GB ram, 25GB disk
 
@@ -27,7 +32,7 @@ resource "vultr_server" "cointhink_algo_1" {
 
 resource "vultr_server" "cointhink_db_1" {
   name = "cointhink db 1"
-  region_id = 4 # Seattle
+  region_id = "${var.region}"
   os_id = 179 #	CoreOS
   plan_id = 201 # $5/mo,1GB ram, 25GB disk
 
