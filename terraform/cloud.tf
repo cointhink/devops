a8065a3adc00126a64f8be0937e3b13839b09b92
Don Park <donp@donp.org>
2017-11-29 11:22:41 +0700

    db1/2 specific yamls. not sure how to parameterize

--- terraform/cloud.tf
+++ terraform/cloud.tf
@@ -42,8 +42,27 @@ resource "vultr_server" "cointhink_db_1" {
   user_data = "${data.template_file.coreos_db.rendered}"
 }
 
+resource "vultr_server" "cointhink_db_2" {
+  name = "vultr-db-2"
+  region_id = "${var.region}"
+  os_id = 179 #	CoreOS
+  plan_id = 201 # $5/mo,1GB ram, 25GB disk
+
+  ipv6 = true
+  private_networking = true
+  ssh_key_ids = ["${vultr_ssh_key.cointhink.id}"]
+  user_data = "${data.template_file.coreos_db2_psql.rendered}"
+}
+
 data "template_file" "coreos_db" {
-  template = "${file("coreos-db.yaml")}"
+  template = "${file("coreos-db1-cockroach.yaml")}"
+  vars {
+    ssh_key = "${vultr_ssh_key.cointhink.public_key}"
+  }
+}
+
+data "template_file" "coreos_db2_psql" {
+  template = "${file("coreos-db2-psql.yaml")}"
   vars {
     ssh_key = "${vultr_ssh_key.cointhink.public_key}"
   }
